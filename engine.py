91224c2173cdce0a2183fe7f06419d8b768f9bc9
StormCloud71 <angie.karagounaki@gmail.com>
2017-09-03 20:50:52 +0300

    new stuff

--- engine.py
+++ engine.py
@@ -1,6 +1,7 @@
 import tdl
 from input_handlers import handle_keys
-from entity import Entity
+from entity import Entity, get_blocking_entities_at_location
+from game_states import GameStates
 from render_functions import clear_all, render_all
 from mapping import make_map, GameMap
 
@@ -13,6 +14,7 @@ def main():
     room_max_size=10
     room_min_size=6
     max_rooms=30
+    max_monsters_per_room = 3
 #FOV configuration
     fov_algorithm='BASIC'
     fov_light_walls=True
@@ -24,25 +26,28 @@ def main():
         'dark_wall': (0, 0, 100),
         'dark_ground': (50, 50, 150),
         'light_wall': (130, 110, 50),
-        'light_ground': (200, 180, 50)
+        'light_ground': (200, 180, 50),
+        'dark_brown' :  (51, 25,0),
+        'slate_gray' :  (112,128,144)
     }
 #end definition
     tdl.set_font('arial10x10.png', greyscale=True, altLayout=True)
     
     
-    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', (255, 255, 255))
-    npc = Entity(int(screen_width / 2), int(screen_height / 2), '@', (0, 255, 0))
-    entities = [npc, player]
+#    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', (255, 255, 255))
+#    npc = Entity(int(screen_width / 2), int(screen_height / 2), '@', (0, 255, 0))
+#    entities = [npc, player]
+    player = Entity(0, 0, '@', (255, 255, 255),'Player',blocks=True)
+    entities = [player]    
     
-    
-
     root_console = tdl.init(screen_width, screen_height, title='Roguelike Tutorial With extra Elbow Grease.')
     con = tdl.Console(screen_width, screen_height)
     game_map = GameMap(map_width, map_height)
     # make_map(game_map)
-    make_map(game_map, max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+    make_map(game_map, max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room, colors)
     
     fov_recompute=True
+    game_state = GameStates.PLAYERS_TURN
     
     while not tdl.event.is_window_closed():
         if fov_recompute:
@@ -71,17 +76,34 @@ def main():
         bye = action.get('exit')
         fullscreen = action.get('fullscreen')
 
-        if move:
+        if move and game_state==GameStates.PLAYERS_TURN:
             dx, dy = move
-            if game_map.walkable[player.x + dx, player.y + dy]:
-                player.move(dx ,dy)
-                fov_recompute=True    
+            destination_x=player.x+dx
+            destination_y=player.y+dy
+            
+            if game_map.walkable[destination_x, destination_y]:
+                target = get_blocking_entities_at_location(entities, destination_x, destination_y)
+                if target:
+                    print('You kick the ' + target.name + ' in the shin, much to their annoyance!')
+                else:
+                    player.move(dx ,dy)
+                    fov_recompute=True
+                    
+                game_state = GameStates.ENEMY_TURN    
+
+        if game_state == GameStates.ENEMY_TURN:
+            for entity in entities:
+                if entity != player:
+                    print('The ' + entity.name + ' ponders the meaning of its existence.')
+
+            game_state = GameStates.PLAYERS_TURN
 
         if bye:
             return True
 
         if fullscreen:
             tdl.set_fullscreen(not tdl.get_fullscreen())
+
 
 if __name__ == '__main__':
     main()
